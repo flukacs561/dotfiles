2ff69258afe152d00c5431aa9aec5a1e644ddc41
Ferenc Lukács <flukacs561@gmail.com>
2024-01-26 20:54:24 +0100

    move latexmkrc into home.nix

--- home.nix
+++ home.nix
@@ -44,7 +44,16 @@ rec {
     };
     packages = with pkgs; [ xdotool ];
     file."latexmkrc" = {
-      source = ./latexmkrc;
+      text = 
+        ''
+          $pdflatex = "pdflatex -halt-on-error %O %S && cp %D %R.pdf";
+          $pdf_mode = 1;
+          $postscript_mode = $dvi_mode = 0;
+          $preview_continuous_mode = 1;
+          
+          $view = 'none';
+          $recorder = 1;
+        '';
       target = "${args.configHome}/latexmk/latexmkrc";
     };
   };
